47f70ae67d006a8d3a2c6db2c0868f3f0654ef99
PrabuddhaRaj <singh.prabuddha@gmail.com>
2021-10-05 16:24:23 -0400

    updated password

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cf api "$INPUT_CF_API"
 cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
-cf login -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD
+cf login -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD"
 cf install-plugin multiapps -f
 
 if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
